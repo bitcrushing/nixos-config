47aefdf72d20fa03cd7fd40aa3345580d6717f88
bitcrushing <kakodaemon@protonmail.com>
2026-06-16 23:06:05 +0100

    opencode config

--- flake.nix
+++ flake.nix
@@ -1,12 +1,41 @@
 {
   description = "nixPC system flake";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    opencode-upstream.url = "github:anomalyco/opencode";
+  };
 
-  outputs = { self, nixpkgs, ... }: {
-    nixosConfigurations.nixPC = nixpkgs.lib.nixosSystem {
+  outputs = { self, nixpkgs, opencode-upstream, ... }@inputs:
+    let
       system = "x86_64-linux";
-      modules = [ ./configuration.nix ];
+
+      opencode-patched = opencode-upstream.packages.${system}.default.overrideAttrs (oldAttrs: {
+        postPatch = (oldAttrs.postPatch or "") + ''
+          if [ -f packages/script/src/index.ts ]; then
+            substituteInPlace packages/script/src/index.ts \
+              --replace-fail "if (!semver.satisfies(process.versions.bun, expectedBunVersionRange))" "if (false)"
+          fi
+        '';
+      });
+
+    in {
+      nixosConfigurations.nixPC = nixpkgs.lib.nixosSystem {
+        inherit system;
+        specialArgs = { inherit inputs; };
+
+        modules = [
+          ./configuration.nix
+          
+          {
+            nixpkgs.overlays = [
+            ( final: prev: {
+              opencode = opencode-patched;
+            }
+            )
+            ];
+        }
+      ];
     };
   };
 }
